a3f075045bf2dc2f6281b6748347e1448f54e243
wetalff <alfredito.osejo@gmail.com>
2025-04-08 10:18:55 -0600

    Ejercicios semana 4

---
+++ Sesion 2/ejercicio1.py
@@ -0,0 +1,8 @@
+#algoritmo que declare tres variables: una para el nombre de una persona, otra para su edad y otra para su estatura.
+Nombre = "Alfredo David Montalvan Osejo"
+Edad = 17
+Estatura = 1.80
+
+print(Nombre)
+
+
