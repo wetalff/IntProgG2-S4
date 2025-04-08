eaf3bc07a2b66b77167f19878ee2dce776469c14
wetalff <alfredito.osejo@gmail.com>
2025-04-08 10:36:30 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio3.py
@@ -0,0 +1,9 @@
+#algoritmo que reciba el nombre y apellido de una persona,
+
+Nombres = input("Dime tus dos nombres: ")
+apellidos = input("Dime tus dos apellidos: ")
+
+nombre_completo = Nombres + " " + apellidos
+
+print("tu nombre completo es: ", nombre_completo)
+
