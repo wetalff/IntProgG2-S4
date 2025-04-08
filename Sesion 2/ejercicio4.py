fec5bfa8bac0307ac31bff80173dc665d390a0a3
wetalff <alfredito.osejo@gmail.com>
2025-04-08 10:47:19 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio4.py
@@ -0,0 +1,8 @@
+#variable de tipo texto que contenga un número (por ejemplo, "25"). Luego convierte ese valor a número e imprímelo incrementado en 1.
+Numero_texto = "25"
+Numero_numero = int(Numero_texto)
+numero_incre = Numero_numero + 1
+print("El numero de texto",Numero_texto, "convertido a numero e incrementado en 1 es:", numero_incre)
+
+
+
