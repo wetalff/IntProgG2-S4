d0665f8388a4ac67adade7223160457456081276
wetalff <alfredito.osejo@gmail.com>
2025-04-08 10:28:00 -0600

    Ejercicios de la semana 4 sesion 2

---
+++ Sesion 2/ejercicios2.py
@@ -0,0 +1,12 @@
+#tres variables numéricas y evalúa la siguiente expresión: (a + b) * c / 2.
+a = 7
+b = 4
+c = 6
+
+Expresion = (a + b) * c / 2
+
+print("El resultado de la expresion: (a + b) * c / 2. Tomando en cuenta que, a=7, b=4, c=6 es: ", Expresion)
+
+print("La expresion se realizo de la siguiente forma: primero asigne los valores de a, b, c, luego le asigne un valor a la expresion y se resuelve primero lo de adentro del parentesis luego la multiplicacion porque esta a la izquierda y por ultimo la division que es lo ultimo a la derecha")
+
+
