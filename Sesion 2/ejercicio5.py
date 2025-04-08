a4110e7116afb52d6fadce3189237b2b0b5c68de
wetalff <alfredito.osejo@gmail.com>
2025-04-08 11:02:58 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio5.py
@@ -0,0 +1,9 @@
+#Algoritmo que calcule el porcentaje de un número dado. Ejemplo: ¿qué es el 15% de 200?
+
+cantidad = float(input("Dime la cantidad a manipular: "))
+porcentaje = float(input("Dime el porcentaje que le quieres sacar a la cantidad mencionada: "))
+porcentaje_aplicado = (porcentaje * cantidad) /100
+
+print("El porcentaje mencionado aplicado a la cantidad proporcionada da un resultado de:", porcentaje_aplicado) 
+
+ 
