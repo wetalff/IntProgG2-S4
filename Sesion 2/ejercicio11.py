0a5a068ca9c6690f2113eb79b2037af152e6bf82
wetalff <alfredito.osejo@gmail.com>
2025-04-08 21:34:20 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio11.py
@@ -0,0 +1,19 @@
+#A partir de las siguientes descripciones, escribe el nombre de una variable que siga una convención lógica y clara. Puedes elegir la convención, pero justificarla.
+
+print("A continuacion mostrare los ejercicios seguido de su respuesta")
+
+print("1. La edad del usuario")
+print("2. El precio total del carrito de compras")
+print("3. El estado de inicio de sesión (activo o inactivo)")
+print("4. El nombre del curso actual")
+print("5. La temperatura promedio registrada")
+
+print("+"*20)
+print("Respuestas")
+print("+"*20)
+
+print("Ejercicio 1 es: edadUsuario, (camelCase) ")
+print("Ejercicio 2 es: precio_tot_carrito, (snake_case) ")
+print("Ejercicio 3 es: Est_inicio_sesion, (snake_case) ")
+print("Ejercicio 4 es: nombre-curso-actual, (kebab-case)  ")
+print("Ejercicio 5 es: tempPromedio, (camelCase) ")
