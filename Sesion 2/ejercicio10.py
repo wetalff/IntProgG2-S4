59c372bd02ac97b054bc4cdf556aa0e035c4a5d8
wetalff <alfredito.osejo@gmail.com>
2025-04-08 21:16:55 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio10.py
@@ -0,0 +1,21 @@
+#Declara nombres de variables para los siguientes casos, aplicando la convención indicada.
+
+print("A continuacion mostrare los ejercicios seguido de su respuesta")
+
+print("1. Nombre completo del estudiante – camelCase")
+print("2. Número de matrícula – snake_case")
+print("3. Total de ventas del mes – PascalCase")
+print("4. Límite máximo de intentos – SCREAMING_SNAKE_CASE")
+print("5. Cantidad de productos – camelCase")
+
+print("+"*20)
+print("Respuestas")
+print("+"*20)
+
+print("Ejercicio 1 es: nombreCompletoEstudiante  ")
+print("Ejercicio 2 es: Numero_de_matricula  ")
+print("Ejercicio 3 es: TotalVentasMes  ")
+print("Ejercicio 4 es: LIMITE_MAXIMO_DE_INTENTOS  ")
+print("Ejercicio 5 es: cantidadProductos  ")
+
+
