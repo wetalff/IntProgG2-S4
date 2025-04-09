5ea19a8224fc1f7d59fc7c493d70e985cd27fbc3
wetalff <alfredito.osejo@gmail.com>
2025-04-08 21:09:13 -0600

    Ejercicios semana 4 sesion 2

---
+++ Sesion 2/ejercicio9.py
@@ -0,0 +1,18 @@
+#Los siguientes nombres de variables están mal escritos o no siguen una convención clara. Corrígelos utilizando camelCase o snake_case, según se indique.
+print("A continuacion mostrare los ejercicios seguido de su respuesta")
+
+print("a) nombrepersona → camelCase")
+print("b) EdadPersona → snake_case")
+print("c) TotalPrecio → camelCase")
+print("d) numero-de-telefono → snake_case")
+print("e) DireccionEmail → camelCase")
+
+print("+"*20)
+print("Respuestas")
+print("+"*20)
+
+print("Ejercicio A es: nombrePersona ")
+print("Ejercicio B es: Edad_Persona ")
+print("Ejercicio C es: totalPrecio ")
+print("Ejercicio D es: numero_de_telefono  ")
+print("Ejercicio E es: direccionEmail ")
